3518e86338aa55e019dc79c2c7681143745b3caf
Jonas Wijne <jonas.wijne@jimber.org>
2020-01-14 09:46:24 +0100

    Make repo standardjs compliant

--- index.js
+++ index.js
@@ -4,65 +4,68 @@ const {
   decodeUTF8,
   encodeUTF8,
   encodeBase64,
-	decodeBase64
+  decodeBase64
 } = require('tweetnacl-util')
+
 class Pkid {
-	constructor(nodeUrl, pk, sk) {
-		this.nodeUrl = nodeUrl
-		this.pk = pk
-		this.sk = sk
-	}
+  constructor (nodeUrl, pk, sk) {
+    this.nodeUrl = nodeUrl
+    this.pk = pk
+    this.sk = sk
+  }
 
-	getDoc(pk, key) {
-		return axios({
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			url: `${this.nodeUrl}/v1/documents/${encodeHex(pk)}/${key}`,
-		}).then(res => {
-			let verified = sign.open(decodeBase64(res.data.data), pk)
-			if (verified) return {
-				data: JSON.parse(encodeUTF8(verified)),
-				verified: true
-			}
-			return {
-				verified: false
-			}
-		}).catch(e => {
-			return e.response.data
-		})
-	}
+  getDoc (pk, key) {
+    return axios({
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${this.nodeUrl}/v1/documents/${encodeHex(pk)}/${key}`
+    }).then(res => {
+      const verified = sign.open(decodeBase64(res.data.data), pk)
+      if (verified) {
+        return {
+          data: JSON.parse(encodeUTF8(verified)),
+          verified: true
+        }
+      }
+      return {
+        verified: false
+      }
+    }).catch(e => {
+      return e.response.data
+    })
+  }
 
-	setDoc(key, payload) {
-		let header = {
-			intent: 'pkid.store',
-			timestamp: (new Date).getTime()
-		}
-	  return axios({
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': signEncode(header, this.sk),
-			},
-			data: JSON.stringify(signEncode(payload, this.sk)),
-			url: `${this.nodeUrl}/v1/documents/${encodeHex(this.pk)}/${key}`
-		}).then(res => {
-			return res.data
-		}).catch(e => {
-			return e.response.data
-		})
-	}
+  setDoc (key, payload) {
+    const header = {
+      intent: 'pkid.store',
+      timestamp: (new Date()).getTime()
+    }
+    return axios({
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: signEncode(header, this.sk)
+      },
+      data: JSON.stringify(signEncode(payload, this.sk)),
+      url: `${this.nodeUrl}/v1/documents/${encodeHex(this.pk)}/${key}`
+    }).then(res => {
+      return res.data
+    }).catch(e => {
+      return e.response.data
+    })
+  }
 }
 
 function signEncode (payload, sk) {
-	let message = decodeUTF8(JSON.stringify(payload))
-	return encodeBase64(sign(message, sk))
+  const message = decodeUTF8(JSON.stringify(payload))
+  return encodeBase64(sign(message, sk))
 }
 
-function encodeHex(byteArray) {
-  return Array.from(byteArray, function(byte) {
-    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+function encodeHex (byteArray) {
+  return Array.from(byteArray, function (byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2)
   }).join('')
 }
 
--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
-const pkid = require('@jimber/pkid')
+const Pkid = require('@jimber/pkid')
 const nacl = require('tweetnacl')
 
-sign = nacl.sign.keyPair()
+const sign = nacl.sign.keyPair()
 
-var client = new pkid('http://localhost:8080', sign.publicKey, sign.secretKey)
+const client = new Pkid('http://localhost:8080', sign.publicKey, sign.secretKey)
 
-let key = 'aaa'
-let payload = 'test'
+const key = 'aaa'
+const payload = 'test'
 
 client.setDoc(key, payload).then(r => {
-    console.log(r)
+  console.log(r)
 }).catch(e => {
-    console.error(e)
+  console.error(e)
 })
 
 setTimeout(() => {
-	client.getDoc(sign.publicKey, key).then(r => {
-		console.log(r)
-	}).catch(e => {
-		console.log(e)
-	})
+  client.getDoc(sign.publicKey, key).then(r => {
+    console.log(r)
+  }).catch(e => {
+    console.log(e)
+  })
 }, 1500)
