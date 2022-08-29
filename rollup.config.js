5b66716c28ac122cc0fdf74df887e592a54fb0dd
Dean Verjans <developer.dean@outlook.com>
2022-08-29 16:02:35 +0200

    Add build support for browser and node

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,34 @@ import typescript from 'rollup-plugin-typescript2';
 import ttypescript from 'ttypescript';
 
 import json from '@rollup/plugin-json';
+import path from 'path'
 
 const config = [
   {
     input: 'src/index.ts',
+    plugins: [
+      typescript({
+        typescript: ttypescript,
+        useTsconfigDeclarationDir: true,
+        emitDeclarationOnly: true,
+      }),
+      json(),
+    ],
     output: {
-      file: 'dist/index.js',
+      file: 'dist/es/index.js',
+      format: 'es',
+      sourcemap: true,
+      exports: 'named',
+    },
+    external: ['axios', 'os', 'url', 'tweetnacl', 'tweetnacl-util', 'libsodium-wrappers'],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/cjs/index.js',
       format: 'cjs',
       sourcemap: true,
-      exports: 'auto'
+      exports: 'named'
     },
     external: ['axios', 'os', 'url', 'tweetnacl', 'tweetnacl-util', 'libsodium-wrappers'],
     plugins: [
